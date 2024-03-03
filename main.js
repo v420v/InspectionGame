b64f5e3c1c63d0a9aa46e20f5686fac19fb7fe96
v420v <ibuki420v@gmail.com>
2024-03-03 12:11:06 +0900

    add time limit

--- main.js
+++ main.js
@@ -1,14 +1,22 @@
+
+const rangeDiv = document.querySelector("#range");
+const sliderDiv = document.querySelector("#slider");
+const scoreDiv = document.getElementById("score");
+const startPage = document.getElementById("start-page");
+const imageInterval = 1500;
+const gameContainer = document.getElementById("game-container");
+
 let score = 0;
+let percent = 0;
+let gameWidth = gameContainer.getBoundingClientRect().width + gameContainer.offsetLeft;
 
 const imageList = [
+  { src: "ng2.png", points: 10 },
   { src: "ok.png", points: -5 },
   { src: "ng.png", points: 10 },
+  { src: "ok.png", points: -5 },
   { src: "ng2.png", points: 10 },
 ];
-
-const imageInterval = 1500;
-
-gameContainer = document.getElementById("game-container");
 
 function resizeWindow() {
   gameContainer.style.height =
@@ -19,19 +27,26 @@ window.addEventListener("resize", resizeWindow);
 
 gameContainer.style.height = gameContainer.getBoundingClientRect().width + "px";
 
-gameWidth =
-  gameContainer.getBoundingClientRect().width + gameContainer.offsetLeft; //window.innerWidth;
-
 function get_game_width() {
   gameWidth =
-    gameContainer.getBoundingClientRect().width + gameContainer.offsetLeft; //window.innerWidth;
+    gameContainer.getBoundingClientRect().width + gameContainer.offsetLeft;
   return gameWidth;
 }
 
 function startGame() {
-  document.getElementById("start-page").style.display = "none";
-  setInterval(() => {
-    createRandomImage();
+  startPage.style.display = "none";
+
+  let updateTimer = setInterval(() => {
+    if (percent >= 100) {
+      clearInterval(updateTimer);
+      document.getElementById("end-page").style.display = "block";
+      document.getElementById("final-score-container").innerHTML = "あなたのスコア<br>" + "<span class=\"final-score\">" + score + "点</span>";
+    } else {
+      percent+=5;
+      sliderDiv.style.width = `${percent}%`
+      setTimeout(updateTimer, 1000);
+      createRandomImage();
+    }
   }, imageInterval);
 }
 
@@ -60,10 +75,10 @@ function createRandomImage() {
 }
 
 function animateImage(imageElement) {
-  let position = get_game_width() - 70;
+  let position = get_game_width() - 60;
 
   const moveImage = () => {
-    position -= 3;
+    position -= 9;
     imageElement.style.left = position + "px";
 
     if (position < gameContainer.offsetLeft) {
@@ -78,8 +93,13 @@ function animateImage(imageElement) {
 
 function updateScore(points) {
   score += points;
-  document.getElementById("score").innerHTML = "スコア : " + score;
-  console.log("Score:", score);
+  if (points < 0) {
+    scoreDiv.innerHTML = "スコア : " + score + "↓";
+    scoreDiv.style.color = "rgba(254, 44, 85, 1)";
+  } else {
+    scoreDiv.innerHTML = "スコア : " + score + "↑";
+    scoreDiv.style.color = "rgb(13, 210, 46)";
+  }
 }
 
 function foundNg() {
@@ -87,4 +107,18 @@ function foundNg() {
     return;
   }
   gameContainer.childNodes[0].dispatchEvent(new Event("click"));
+}
+
+function init() {
+  score = 0;
+  scoreDiv.innerHTML = "スコア : " + score;
+  scoreDiv.style.color = "rgb(47, 47, 47)";
+  sliderDiv.style.width = 0;
+  percent = 0;
+}
+
+function playAgain() {
+  init();
+  document.getElementById("end-page").style.display = "none";
+  startGame();
 }
